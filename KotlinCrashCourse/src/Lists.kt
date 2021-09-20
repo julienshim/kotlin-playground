b5dabaed0636bb5030c80bdc6faacba65f5de467
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-20 13:50:02 -0700

    lists

---
+++ KotlinCrashCourse/src/Lists.kt
@@ -0,0 +1,7 @@
+fun main() {
+//    val shoppingList = listOf<String>("Lamborghini", "Penthouse", "Rolex") // String not necessary
+    val shoppingList = mutableListOf("Lamborghini", "Penthouse", "Rolex")
+    shoppingList.add("Ferrari")
+//    println(shoppingList[0])
+    println(shoppingList[3])
+}
