603bd60087712015696dffa6fbcb228c44164706
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-19 23:38:39 -0700

    if cond

---
+++ KotlinCrashCourse/src/IfConditions.kt
@@ -0,0 +1,15 @@
+fun main() {
+//    val x = 1 + 1
+//    val x = 1 + 2
+    val x = 2 + 2
+    if(x == 2) {
+        println("x is 2")
+    } else if(x == 3) (
+        println("x is 3")
+    ) else {
+        println("x is not 2 or 3")
+    }
+
+    val y = if(x==2) 2 else 3
+    println(y)
+}
