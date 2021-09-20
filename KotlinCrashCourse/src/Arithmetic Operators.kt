252fee1ddb8cbc822d1a9ca731d2a8f32eb288e4
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-19 23:37:55 -0700

    arth op

---
+++ KotlinCrashCourse/src/Arithmetic Operators.kt
@@ -0,0 +1,12 @@
+fun main() {
+//    val x = 3 * 4 // + -
+//    val x = 3 / 4 // Whole numbers -> Integer = 0
+//    val x = 3f / 4f // F -> Float 0.75
+    val x  = 10 % 2 // 0
+    println("The value of x is $x")
+
+    val amIAnAdult = true
+    val amIAProgrammer = true
+    val amIAnAdultProgrammer = amIAnAdult && amIAProgrammer // || or == double equals != not equal ! negate
+    println(amIAnAdultProgrammer)
+}
